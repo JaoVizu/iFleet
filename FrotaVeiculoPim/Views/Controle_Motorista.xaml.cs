9d7f731c9d109bfe96bb6702a1702a99ac63a8a2
JoaoVizu <joaovizu2015@gmail.com>
2019-06-02 20:04:16 -0300

    Modulo motorista

--- FrotaVeiculoPim/Views/Controle_Motorista.xaml.cs
+++ FrotaVeiculoPim/Views/Controle_Motorista.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ControllerMotorista.models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,6 +28,37 @@ namespace FrotaVeiculoPim.Views
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void BtnListaMotorista_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnSalvar_Click(object sender, RoutedEventArgs e)
+        {
+            string exameMedico;
+            if (rbNao.IsChecked == true)
+                exameMedico = "NAO";
+            else
+                exameMedico = "SIM";
+
+            Motorista motorista = new Motorista()
+            {
+                Nome = txtNome.Text,
+                Cpf = txtCpf.Text,
+                Cnh = txtCnh.Text,
+                CategoriaCnh = txtCatCnh.Text,
+                DataNascimento = dtDataNasc.SelectedDate.Value,
+                ExameMedico = exameMedico,
+                Email = txtEmail.Text
+            };
+            motorista.Endereco = new Endereco();
+            motorista.Endereco.Rua = txtRua.Text;
+            motorista.Endereco.Bairro = txtBairro.Text;
+            motorista.Endereco.Numero = txtNumero.Text;
+            motorista.Endereco.Cidade = txtCidade.Text;
+            motorista.Endereco.Cep = txtCep.Text;
         }
     }
 }
